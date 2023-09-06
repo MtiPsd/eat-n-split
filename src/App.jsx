3dbbaf7d9a3dec6aaa3a380ca937eb5d49278346
Mahdi Pasandideh <mtipsd.git@gmail.com>
2023-09-06 17:22:28 +0330

    interactivity :: add new friend

--- src/App.jsx
+++ src/App.jsx
@@ -30,17 +30,25 @@ function Button({ children, onClick }) {
 }
 
 export default function App() {
+  const [friends, setFriends] = useState([]);
   const [showAddFriend, setShowAddFriend] = useState(false);
 
   function handleShowAddFriend() {
     setShowAddFriend(show => !show);
   }
 
+  function handleAddFriend(friend) {
+    setFriends(friends => [...friends, friend]);
+    setShowAddFriend(false);
+  }
+
   return (
     <div className='app'>
       <div className='sidebar'>
-        <FriendsList />
-        {showAddFriend && <FormAddFriend />}
+        <FriendsList friends={friends} />
+        {showAddFriend && (
+          <FormAddFriend onAddFriend={handleAddFriend} />
+        )}
         <Button onClick={handleShowAddFriend}>
           {showAddFriend ? 'Close' : 'Add friend'}
         </Button>
@@ -51,8 +59,7 @@ export default function App() {
   );
 }
 
-function FriendsList() {
-  const friends = initialFriends;
+function FriendsList({ friends }) {
   return (
     <ul>
       {friends.map(friend => (
@@ -87,21 +94,27 @@ function Friend({ friend }) {
   );
 }
 
-function FormAddFriend() {
+function FormAddFriend({ onAddFriend }) {
   const [name, setName] = useState('');
-  const [image, setImage] = useState(
-    'https://i.pravatar.cc/48?u=499476',
-  );
+  const [image, setImage] = useState('https://i.pravatar.cc/48');
 
   function handleSubmit(e) {
     e.preventDefault();
+
+    if (!name || !image) return;
+
+    const id = crypto.randomUUID();
     const newFriend = {
       name,
-      image,
+      image: `${image}?u=${id}`,
       balance: 0,
-      id: crypto.randomUUID(),
+      id,
     };
+    onAddFriend(newFriend);
+    setName('');
+    setImage('https://i.pravatar.cc/48');
   }
+
   return (
     <form className='form-add-friend' onSubmit={handleSubmit}>
       <label>Friend name</label>
