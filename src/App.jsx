c4cd4036d128902cc90b20a432dbd60cf5cfae0a
Mahdi Pasandideh <mtipsd.git@gmail.com>
2023-09-06 13:53:54 +0330

    Friend & FriendList component implementation done

--- src/App.jsx
+++ src/App.jsx
@@ -47,6 +47,22 @@ function Friend({ friend }) {
     <li>
       <img src={friend.image} alt={friend.name} />
       <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && (
+        <p className='red'>
+          You own {friend.name} {Math.abs(friend.balance)}$
+        </p>
+      )}
+
+      {friend.balance > 0 && (
+        <p className='green'>
+          {friend.name} owens You {Math.abs(friend.balance)}$
+        </p>
+      )}
+
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+
+      <button className='button'>Select</button>
     </li>
   );
 }
